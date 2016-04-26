2ebea087f18e957946c40b291f3b7479e579dafe
e3matheus <e3matheus@gmail.com>
2016-04-26 11:48:16 -0500

    Sum all mobile and all desktop purchases

--- jobs/mobile_vs_desktop_search.rb
+++ jobs/mobile_vs_desktop_search.rb
@@ -3,7 +3,11 @@ client = Mixpanel::Api.new
 SCHEDULER.every '10m', :first_in => 0 do
   today = Date.today.strftime
   data = client.segmentation("Purchase Complete", 'properties["product"]', today, today)
-  first_current = data["web"].values.first
-  second_current = data["web-mobile"].values.first
+  first_current = ["web", "undefined"].inject(0) { |sum, product|
+    sum + data[product].try(:values).try(:first).to_i
+  }
+  second_current = ["web-mobile", "ios-cordova", "android-cordova"].inject(0) { |sum, product|
+    sum + data[product].try(:values).try(:first).to_i
+  }
   send_event('mobilevsdesktop', first_current: first_current, second_current: second_current)
 end

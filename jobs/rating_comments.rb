7c1b6ec58520189629ffd91bb873ec152266d069
e3matheus <e3matheus@gmail.com>
2016-04-26 10:47:28 -0500

    Adjust size of dashboard to fit more columns. Add rating comment widget

---
+++ jobs/rating_comments.rb
@@ -0,0 +1,8 @@
+require 'rest-client'
+
+SCHEDULER.every '60s' do
+  response = Dataclip::Api.items("***REMOVED***")
+  sample_row = response.sample
+
+  send_event('rating_comments', { title: "Comentario de #{sample_row.fetch("line")}", text: sample_row.fetch("comment") })
+end
